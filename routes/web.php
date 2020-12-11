faa389a70c90bc01c6a0d8be1c5a963e176def18
李譯騏 <patrick14754@gmail.com>
2020-12-11 22:55:37 +0800

    錯誤重作: 1.使用artisan命令產生三個Controller，並把三個路由對應三個Controller 2.三個view各新增2個route按鈕連接

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
-
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\NewsController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,7 +14,6 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index'])->name('home.index');
+Route::get('about', [AboutController::class, 'index'])->name('about.index');
+Route::get('news', [NewsController::class, 'index'])->name('news.index');
